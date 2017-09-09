a21e785558e027fced3c8ecef0641755d1ae0b70
AraksiaMilek <31700503+AraksiaMilek@users.noreply.github.com>
2017-09-09 16:00:56 +0400

    javascript

---
+++ homework2.js
@@ -0,0 +1,21 @@
+//variables
+const x = 5
+const y = 10
+const z = x + y
+
+//arithmetic operations printed to the console
+console.log (x+y);
+console.log ((x+y) * y);
+console.log (x/(y-x)) //probably would show NaN
+console.log (z*5)
+
+
+if (x=y) {
+
+	console.log ("you can't divide by zero");
+}   else {
+
+	console.log ("")
+}
+
+
